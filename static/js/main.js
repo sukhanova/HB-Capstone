4f9625bd4357e89d771c0bd921e6fdd79e672182
Vira Sukhanova <verusia.s@gmail.com>
2021-07-24 15:06:19 -0500

    Fixed spacing between note and Remove button on POST

--- static/js/main.js
+++ static/js/main.js
@@ -12,7 +12,7 @@ $(document).ready(function() {
 		};
 		const userNoteReq = $.ajax(payload);
 		userNoteReq.then(function(res) {
-			const htmlToAdd = `<div>${res.note}<button data-source="{{note.note_id}}" type="submit" class="btn btn-outline-secondary btn-sm remove"><i class="fa fa-trash fa-1" aria-hidden="true">Remove</i></button></div>`
+			const htmlToAdd = `<div>${res.note}<br><button data-source="{{note.note_id}}" type="submit" class="btn btn-outline-secondary btn-sm remove"><i class="fa fa-trash fa-1" aria-hidden="true">Remove</i></button></>`
 			userNotes.append(htmlToAdd);
 			$('#note').val('');
 		})
