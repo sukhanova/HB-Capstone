6eca6f683dc8309ef51e1c7b423a905be5fcd475
Vira Sukhanova <verusia.s@gmail.com>
2021-07-24 19:01:51 -0500

    Working on styling

--- static/js/main.js
+++ static/js/main.js
@@ -12,7 +12,7 @@ $(document).ready(function() {
 		};
 		const userNoteReq = $.ajax(payload);
 		userNoteReq.then(function(res) {
-			const htmlToAdd = `<div>${res.note}<br><button data-source="{{note.note_id}}" type="submit" class="btn btn-outline-secondary btn-sm remove"><i class="fa fa-trash fa-1" aria-hidden="true">Remove</i></button></>`
+			const htmlToAdd = `<p>${res.note}<br><button data-source="{{note.note_id}}" type="submit" class="btn btn-outline-secondary btn-sm remove"><i class="fa fa-trash fa-1" aria-hidden="true">Remove</i></button></p>`
 			userNotes.append(htmlToAdd);
 			$('#note').val('');
 			window.location.reload();
